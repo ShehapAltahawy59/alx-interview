8d658a270f87d85c307945fdfa3190a5a325434c
shehap62 <shehapkhalil62@gmail.com>
2024-07-25 15:40:16 +0300

    edit

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -10,13 +10,14 @@ def pascal_triangle(n):
     """
     if n <= 0 :
         return[]
-    pascal_triangle=[]
-
+    triangle = []
     for row in range(n):
-        currunt_row = [1]*(row+1)
-        print(currunt_row)
-        for j in range(1,row):
-            currunt_row[j] = pascal_triangle[row-1][j-1]+pascal_triangle[row-1][j]
-        pascal_triangle.append(currunt_row)
-    return pascal_triangle
+        # Initialize each row with 1s
+        current_row = [1] * (row + 1)
+        # Calculate values between the edges of the triangle
+        for j in range(1, row):
+            current_row[j] = triangle[row - 1][j - 1] + triangle[row - 1][j]
+        triangle.append(current_row)
+    return triangle
 
+print(pascal_triangle(5))
