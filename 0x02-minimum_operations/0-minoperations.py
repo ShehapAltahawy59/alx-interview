cad593932c579dcd705beabb5d808d30a89fdff9
shehap62 <shehapkhalil62@gmail.com>
2024-08-11 22:55:14 +0300

    edit

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -19,11 +19,9 @@ def minOperations(n):
 
     operations = 0
     factor = 2
-    
     while n > 1:
         while n % factor == 0:
             operations += factor
             n //= factor
         factor += 1
-    
     return operations
